5cbfbee8522203e3d5480ea31ada2ba596556977
Mythsoul <Mythsoul2023@gmail.com>
2024-12-03 15:27:11 +0545

    starting

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+
+function App() {
+console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+     
+      <h1 className='text-3xl text-center'>HI</h1>
+    </>
+  )
+}
+
+export default App
