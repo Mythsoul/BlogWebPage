82cf4e964251eef1fb410d7f3e2545c249e424e2
Mythsoul <Mythsoul2023@gmail.com>
2024-12-10 17:40:50 +0545

    test

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Select from "./Select";
-import Header from "./header/Header.jsx"
+import Header from "./header/Header"
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
@@ -7,11 +7,11 @@ import LogoutBtn from "./header/LogoutBtn.jsx";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
-import Button from "./Button";
-import PostForm from "./post-form/PostForm";
-import PostCard from "./PostCard";
-import AuthLayout from "./AuthLayout";
-import Input from "./Input";
+import Button from "./Button.jsx";
+import PostForm from "./post-form/PostForm.jsx";
+import PostCard from "./PostCard.jsx"
+import AuthLayout from "./AuthLayout.jsx";
+import Input from "./Input.jsx"
 export {
     Header,
     Footer,
